15610b196f91fc32209c5f2657d082f5fa69a15e
Parker Parker <792797@aacps.org>
2020-10-18 13:51:40 +0000

    fixed the print issue

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,26 +1,26 @@
 public class CardRunner {
     public static void main(String args[]){
-        // ☕
+        
         TerrapinCard cardBilly = new TerrapinCard(20);
         TerrapinCard cardBrian = new TerrapinCard(30);
-        double billy = cardBilly.getBalence();
-        double brian = cardBrian.getBalence();
+        
         //cardBilly and cardBrian can be replaced with billy and brian, however code wise this would be inefficent as the variables billy and brian would have to be redefined after every change to the balence;
         // System.out.println(billy);
         // System.out.println(brian);
         cardBilly.payGourmet();
-        System.out.println(cardBilly);
+        System.out.println("Billy: " + cardBilly);
         cardBrian.payEconomical();
-        System.out.println(cardBrian);
+        System.out.println("Brian: " + cardBrian);
         cardBilly.loadMoney(20);
-        System.out.println(cardBilly);
+        System.out.println("Billy: " + cardBilly);
         cardBrian.payGourmet();
-        System.out.println(cardBrian);
+        System.out.println("Brian: " + cardBrian);
         cardBilly.payEconomical();
         cardBilly.payEconomical();
-        System.out.println(cardBilly);
+        System.out.println("Billy: " + cardBilly);
         cardBrian.loadMoney(50);
-        System.out.println(cardBrian);
+        System.out.println("Brian: " + cardBrian);
+        
         //economic = -2.5, gourmet = -4
         
     }
